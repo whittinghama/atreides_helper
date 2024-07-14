9ddc4a0a90ff5e5f4582c8ef1cbb82f97abde74b
Andrew Whittigham <whittingham.ar@gmail.com>
2024-07-14 11:52:57 +0100

    add dialog for dynamic number of factionLayouts to add

--- factionLayout.py
+++ factionLayout.py
@@ -1,15 +1,17 @@
-from PyQt6.QtWidgets import QWidget, QHBoxLayout, QPlainTextEdit, QPushButton
+from PyQt6.QtWidgets import QHBoxLayout, QPlainTextEdit, QLabel
+from PyQt6.QtGui import QPixmap
+
 from cardLayout import CardLayout
 
-class FactionWidget(QWidget):
-    def __init__(self):
-        super().__init__()
-  
 
 class FactionLayout(QHBoxLayout):
-    def __init__(self):
+    def __init__(self, faction: str):
         super().__init__()
-        self.addWidget(FactionWidget())
+
+        self.image = QLabel()
+        self.image.setPixmap(QPixmap(f"images/{faction}.png"))
+
+        self.addWidget(self.image)
         for _ in range(4):
             self.addLayout(CardLayout())
         self.addWidget(QPlainTextEdit())
--- mainWindow.py
+++ mainWindow.py
@@ -1,9 +1,30 @@
-from PyQt6.QtWidgets import QWidget, QMainWindow, QVBoxLayout
+from PyQt6.QtWidgets import QWidget, QMainWindow, QVBoxLayout, QDialogButtonBox, QFormLayout, QDialog, QCheckBox
 from PyQt6.QtGui import QIcon
 
 from factionLayout import FactionLayout
 
-# Subclass QMainWindow to customize your application's main window
+
+class FactionDialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Select Factions")
+        self.setWindowIcon(QIcon("images/atr_logo_256x"))
+
+        self.layout = QVBoxLayout()
+
+        self.factionBoxes = [QCheckBox("Bene Gesserit"), QCheckBox("Emperor"), QCheckBox("Fremen"), QCheckBox("Harkonnen"), QCheckBox("Spacing Guild")]
+        for box in self.factionBoxes:
+            self.layout.addWidget(box)
+
+        self.buttons = QDialogButtonBox(QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel)
+        self.buttons.accepted.connect(self.accept)
+        self.buttons.rejected.connect(self.reject)
+
+        self.layout.addWidget(self.buttons)
+
+        self.setLayout(self.layout)
+
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -11,9 +32,21 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Atreides Helper")
         self.setWindowIcon(QIcon("images/atr_logo_256x.png"))
 
+        dlg = FactionDialog(self)
+
+        factions = []
+        if dlg.exec():
+            for box in dlg.factionBoxes:
+                if box.isChecked():
+                    factions.append(box.text())
+        else:
+            exit()
+
         layout = QVBoxLayout()
-        for _ in range(4):
-            layout.addLayout(FactionLayout())
+        for faction in factions:
+            layout.addLayout(FactionLayout(faction))
+            if faction == "Harkonnen":
+                layout.addLayout(FactionLayout(faction)) # Add a second row for Harkonnen since they can have 8 cards
 
         widget = QWidget()
         widget.setLayout(layout)
