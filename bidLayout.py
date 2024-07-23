6f1253dd958c05a4326333406b014424a78fbd85
Andrew Whittingham <whittingham.ar@gmail.com>
2024-07-23 20:41:07 +0100

    stats layout improved

--- bidLayout.py
+++ bidLayout.py
@@ -1,5 +1,5 @@
 from PyQt6.QtCore import pyqtSignal, Qt
-from PyQt6.QtWidgets import QHBoxLayout, QVBoxLayout, QLabel
+from PyQt6.QtWidgets import QHBoxLayout, QGridLayout, QLabel
 
 from cardLayout import CardLayout
 from spiceLayout import SpiceLayout
@@ -12,7 +12,7 @@ class BidLayout(QHBoxLayout):
         super().__init__()
 
         self.cardLayouts = []
-        self.statsLayout = QVBoxLayout()
+        self.statsLayout = QGridLayout()
 
         for i in range(5):
             self.cardLayouts.append(CardLayout(factions))
@@ -24,18 +24,26 @@ class BidLayout(QHBoxLayout):
         statsTitleFont.setBold(True)
         statsTitleFont.setPointSize(14)
         self.statsTitle.setFont(statsTitleFont)
-        self.statsLayout.addWidget(self.statsTitle, Qt.AlignmentFlag.AlignCenter)
+        self.statsLayout.addWidget(self.statsTitle, 0, 0, 1, 2, alignment=Qt.AlignmentFlag.AlignCenter)
 
         self.statsLabels = {}
+        row = 1
         for stat in statsKeys:
-            labelText = f"{stat}: Unknown"
-            label = QLabel(labelText)
-            self.statsLabels[stat] = label
-            self.statsLayout.addWidget(self.statsLabels[stat], Qt.AlignmentFlag.AlignCenter)  
+            statLabelText = f"{stat}:"
+            statLabel = QLabel(statLabelText)
+            valueLabel = QLabel("TBD %")
+
+            self.statsLabels[stat] = (statLabel, valueLabel)
+            self.statsLayout.addWidget(self.statsLabels[stat][0], row, 0, alignment=Qt.AlignmentFlag.AlignRight)
+            self.statsLayout.addWidget(self.statsLabels[stat][1], row, 1, alignment=Qt.AlignmentFlag.AlignLeft)
+            row += 1
 
         self.addLayout(self.statsLayout)
         
     def updateStats(self, stats: dict):
         for stat, value in stats.items():
-            labelText = f"{stat}: {value}"
-            self.statsLabels[stat].setText(labelText)
+            if stat == "Remaining Cards" or stat == "Unknown Cards":
+                valueLabelText = f"{value}"
+            else:
+                valueLabelText = f"{value:5.2f} %"
+            self.statsLabels[stat][1].setText(valueLabelText)
